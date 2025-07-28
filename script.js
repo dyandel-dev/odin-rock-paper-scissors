571b014673f05ae6cf3fda3b0252136880b9d89f
JD <117078422+dyandel-dev@users.noreply.github.com>
2025-07-28 15:34:36 +0800

    Create getComputerChoice function based on random number
    Implemented getComputerChoice to return 'rock', 'paper',
    or 'scissors' by mapping assigned numbers to each option
    using a generated random value.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+function getComputerChoice() {
+  let randomNum = getRandomInt(3);
+  let choice;
+
+  if (randomNum === 0) {
+    choice = "Rock";
+  } else if (randomNum === 1) {
+    choice = "Paper";
+  } else {
+    choice = "Scissors";
+  }
+
+  return choice;
+}
